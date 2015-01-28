f12ec77c152cb592b9b948afb155ad56ae2c48d4
Andrea Stella <adr.stella@gmail.com>
2015-01-28 00:06:08 -0600

    committing for the night, still broken

--- hide.js
+++ hide.js
@@ -1,22 +1,19 @@
 
+var blacklistCache;
+
 // create an observer instance
-var observer = new MutationObserver(function(mutations)
+var observer = new MutationObserver(function()
 {
-	$('.boxart:not(.thg-processed)').each(function(i, boxart)
-	{
-		var closeButton = $('<div class="thg-hide-button">');
-		$(boxart).addClass('thg-processed').append(closeButton);
-		closeButton.on('click', function(e)
-		{
-			var game = $(e.currentTarget).closest('a').attr('original-title');
-			blacklist(game);
-			e.stopImmediatePropagation();
-			e.preventDefault();
-		});
-	})
+	addHideButtons();
+	if (blacklistCache) hideBlacklisted(blacklistCache);
 });
 
 observer.observe(document, {childList: true, subtree: true});
+
+blacklist(function(list)
+{
+	blacklistCache = list;
+});
 
 function blacklist(arg1, arg2)
 {
@@ -27,9 +24,13 @@ function blacklist(arg1, arg2)
 		{
 			var blacklist = list || [];
 			blacklist.push(arg1);
+			blacklist = blacklist.sort().filter(function(element, index, array) {
+				return element !== array[index - 1]
+			});
+			console.log('BLACKLIST', blacklist);
 			storageSet('blacklist', blacklist);
-			console.log('LIST', blacklist);
 			hideBlacklisted(blacklist);
+			blacklistCache = blacklist;
 			if (arg2) arg2(blacklist);
 		}
 	});
@@ -39,8 +40,30 @@ function hideBlacklisted(blacklist)
 {
 	blacklist.forEach(function(game)
 	{
-		$('[original-title="'+game+'"]').closest('.stream').hide();
+		var selector = 'a.boxart[title="'+game+'"], a.boxart[original-title="'+game+'"]';
+		$(selector).each(function(i, element)
+		{
+			$(element).closest('.stream').hide();
+		});
 	});
+}
+
+function addHideButtons()
+{
+	$('.boxart:not(.thg-processed)').each(function(i, boxart)
+	{
+		var closeButton = $('<div class="thg-hide-button">');
+		$(boxart).addClass('thg-processed').append(closeButton);
+		closeButton.on('click', blacklistFromButton);
+	});
+}
+
+function blacklistFromButton(event)
+{
+	var game = $(event.currentTarget).closest('a').attr('original-title');
+	blacklist(game);
+	event.stopImmediatePropagation();
+	event.preventDefault();
 }
 
 function unBlackList(game)
