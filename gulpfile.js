257bd3f2bbc2570cda979a458b9b5a7603b6227b
Andrea Stella <adr.stella@gmail.com>
2015-02-01 20:54:10 -0600

    fixed build bug, it wasn't cleaning the dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp');
 var zip = require('gulp-zip');
+var clean = require('gulp-clean');
 
-gulp.task('make-zip', function()
+gulp.task('make-zip', ['clean-dist'], function()
 {
 	return gulp.src('src/*')
 		.pipe(zip('dist_store.zip'))
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['make-zip']);
+gulp.task('clean-dist', function()
+{
+	return gulp.src('./dist', {read: false}).pipe(clean());
+});
+
+gulp.task('default', ['clean-dist', 'make-zip']);
